fc66a4a03516ebf107005430e8a51e31aa1ac514
zachariaszstrus <zachariasz.strus@gmail.com>
2020-05-04 22:05:29 +0200

    circleci project setup

--- src/screens/Example/Example.component.tsx
+++ src/screens/Example/Example.component.tsx
@@ -6,9 +6,7 @@ const ExampleComponent = () => {
   return (
     <Grid container direction="column" justify="center" alignItems="center">
       <img src={logo} alt="logo" height={100} />
-      <Typography>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </Typography>
+      <Typography>test</Typography>
       <Link
         href="https://reactjs.org"
         target="_blank"
