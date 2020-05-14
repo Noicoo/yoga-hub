3fc1d8b4e7de07dd82351998f0b057cf4ca77a94
zaki <zachariasz.strus@gmail.com>
2020-05-14 17:31:02 +0200

    redux config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Example from './screens/Example';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 
 const App = () => {
-  return <Example />;
+  return (
+    <Provider store={store}>
+      <Example />
+    </Provider>
+  );
 };
 
 export default App;
