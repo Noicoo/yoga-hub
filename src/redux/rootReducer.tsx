d244690364f2a871b25a6e0f73e43d164051f02a
zaki <zachariasz.strus@gmail.com>
2020-05-19 17:25:58 +0200

    In rootReducer.tsx: subsituted - import userReducer from './user/index'; with - import userReducer from './user';

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,5 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/index';
+import userReducer from './user';
 
 const rootReducer = combineReducers({
   user: userReducer,
