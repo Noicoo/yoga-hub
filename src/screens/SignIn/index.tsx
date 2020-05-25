770826450414350accd6fc86d244c4ae74b53bfd
zaki <zachariasz.strus@gmail.com>
2020-05-25 16:37:37 +0200

    - firebase initialization

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,13 @@
 import React, { FC } from 'react';
+import fire from '../../config/services/firebase';
 
-const SignIn: FC = () => <div>Sign In</div>;
+const SignIn: FC = () => (
+  <div
+    onClick={() => {
+      console.log(fire);
+    }}>
+    Sign In
+  </div>
+);
 
 export default SignIn;
