5501f1fab78ee3631e50e466ed00108c36230235
zaki <zachariasz.strus@gmail.com>
2020-07-21 19:35:01 +0200

    I created the basic component used for adding video: - you can only use youtube videos (done by using a regex) - from the video the video id is extracted (necessary to use the youtube api) - the video id is set in the local component's state
    TO BE DONE:
    
    - the id will be used to get the object related to the video (thumbnail, duration, name and so on) via the YT api
    - the object will be added to an array which will contain our list of videos (I have to check out how that can be done via Firestore)
    - redux's store will take it's video list values from firestore

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 export const homePagePath = '/';
 export const signInPath = '/sign-in';
 export const signUpPath = '/sign-up';
+export const addVideoPath = '/add-video';
 export const resetPasswordPath = '/reset-password';
 export const deleteAccountPath = '/delete-account';
 export const selectedVideoPath = (videoId: string) => `/videos/${videoId}`;
